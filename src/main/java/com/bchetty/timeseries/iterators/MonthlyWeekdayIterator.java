d282ced54fc31412fab37bb6395173cc6e65f468
Babji Prashanth, Chetty <dev.b.chetty@gmail.com>
2015-06-11 15:10:12 +0200

    Cleared some bugs

--- src/main/java/com/bchetty/timeseries/iterators/MonthlyWeekdayIterator.java
+++ src/main/java/com/bchetty/timeseries/iterators/MonthlyWeekdayIterator.java
@@ -17,30 +17,39 @@ public class MonthlyWeekdayIterator implements TimeSeriesIterator {
     private DateTime currentDateTime;
     private MonthlyWeekday monthlyWeekday;
     private int monthIncrement;
-    
+
     public MonthlyWeekdayIterator(TimeSeries timeSeries) {
-        if(timeSeries != null && timeSeries.getBeginDate() != null && timeSeries.getEndDate() != null) {            
+        if (timeSeries != null && timeSeries.getBeginDate() != null && timeSeries.getEndDate() != null) {
             this.currentDateTime = new DateTime(timeSeries.getBeginDate());
             this.endDateTime = new DateTime(timeSeries.getEndDate());
             TimeSeries.Monthly monthly = timeSeries.getMonthly();
-            if(monthly != null) {
+            if (monthly != null) {
                 monthlyWeekday = monthly.getMonthlyWeekday();
-                if(monthlyWeekday != null) {
+                if (monthlyWeekday != null) {
                     monthIncrement = monthlyWeekday.getIncrement();
-                    Month month = Month.values()[currentDateTime.getMonthOfYear()];
-                    currentDateTime = DateTimeUtils.getNthWeekdayOfMonth(currentDateTime.getYear(), month, 
+                    Month month = Month.values()[currentDateTime.getMonthOfYear() - 1];
+                    currentDateTime = DateTimeUtils.getNthWeekdayOfMonth(currentDateTime.getYear(), month,
                             monthlyWeekday.getWeekday(), monthlyWeekday.getWeekOfMonth());
                     DateTime startDateTime = new DateTime(timeSeries.getBeginDate());
-                    if(currentDateTime.isBefore(startDateTime)) {
-                        currentDateTime = currentDateTime.plusMonths(monthIncrement);
+                    if (currentDateTime.isBefore(startDateTime)) {
+                        int monthEnumOrdinal = (currentDateTime.getMonthOfYear() - 1) + monthIncrement;
+                        int year = currentDateTime.getYear();
+                        if (monthEnumOrdinal >= 12) {
+                            monthEnumOrdinal = (monthEnumOrdinal - 12);
+                            year += 1;
+                        }
+
+                        Month nextMonth = Month.values()[monthEnumOrdinal];
+                        currentDateTime = currentDateTime = DateTimeUtils.getNthWeekdayOfMonth(year, nextMonth,
+                                monthlyWeekday.getWeekday(), monthlyWeekday.getWeekOfMonth());
                     }
                 }
-            }            
+            }
         } else {
             throw new IllegalStateException();
         }
     }
-    
+
     @Override
     public boolean hasNext() {
         return !currentDateTime.isAfter(endDateTime);
@@ -48,24 +57,24 @@ public class MonthlyWeekdayIterator implements TimeSeriesIterator {
 
     @Override
     public Date next() {
-        if(this.hasNext()) {
+        if (this.hasNext()) {
             Date currentDate = currentDateTime.toDate();
-            int monthEnumOrdinal = (currentDateTime.getMonthOfYear()-1) + monthIncrement;
+            int monthEnumOrdinal = (currentDateTime.getMonthOfYear() - 1) + monthIncrement;
             int year = currentDateTime.getYear();
-            if(monthEnumOrdinal >= 12) {
+            if (monthEnumOrdinal >= 12) {
                 monthEnumOrdinal = (monthEnumOrdinal - 12);
                 year += 1;
             }
-            
+
             Month month = Month.values()[monthEnumOrdinal];
-            currentDateTime = DateTimeUtils.getNthWeekdayOfMonth(year, month, monthlyWeekday.getWeekday(), 
+            currentDateTime = DateTimeUtils.getNthWeekdayOfMonth(year, month, monthlyWeekday.getWeekday(),
                     monthlyWeekday.getWeekOfMonth());
             return currentDate;
         }
-        
+
         throw new NoSuchElementException();
     }
-    
+
     @Override
     public void remove() {
         throw new UnsupportedOperationException();
--- src/main/java/com/bchetty/timeseries/iterators/YearlyWeekdayIterator.java
+++ src/main/java/com/bchetty/timeseries/iterators/YearlyWeekdayIterator.java
@@ -2,6 +2,7 @@ package com.bchetty.timeseries.iterators;
 
 import com.bchetty.timeseries.TimeSeries;
 import com.bchetty.timeseries.TimeSeries.Yearly.YearlyWeekday;
+import com.bchetty.timeseries.utils.enums.Month;
 import com.bchetty.timeseries.utils.misc.DateTimeUtils;
 import java.util.Date;
 import java.util.NoSuchElementException;
@@ -16,28 +17,29 @@ public class YearlyWeekdayIterator implements TimeSeriesIterator {
     private DateTime currentDateTime;
     private int yearIncrement = 1;
     private YearlyWeekday yearlyWeekday;
-    
+
     public YearlyWeekdayIterator(TimeSeries timeSeries) {
-        if(timeSeries != null && timeSeries.getBeginDate() != null && timeSeries.getEndDate() != null) {
+        if (timeSeries != null && timeSeries.getBeginDate() != null && timeSeries.getEndDate() != null) {
             this.currentDateTime = new DateTime(timeSeries.getBeginDate());
             this.endDateTime = new DateTime(timeSeries.getEndDate());
             TimeSeries.Yearly yearly = timeSeries.getYearly();
-            if(yearly != null) {
+            if (yearly != null) {
                 yearlyWeekday = yearly.getYearlyWeekday();
-                if(yearlyWeekday != null) {
-                    currentDateTime = DateTimeUtils.getNthWeekdayOfMonth(currentDateTime.getYear(), yearlyWeekday.getMonth(), 
+                if (yearlyWeekday != null) {                    
+                    currentDateTime = DateTimeUtils.getNthWeekdayOfMonth(currentDateTime.getYear(), yearlyWeekday.getMonth(),
                             yearlyWeekday.getWeekday(), yearlyWeekday.getWeekOfMonth());
                     DateTime startDateTime = new DateTime(timeSeries.getBeginDate());
-                    if(currentDateTime.isBefore(startDateTime)) {
-                        currentDateTime = currentDateTime.plusYears(yearIncrement);
+                    if (currentDateTime.isBefore(startDateTime)) {
+                        currentDateTime = DateTimeUtils.getNthWeekdayOfMonth(currentDateTime.getYear() + yearIncrement, yearlyWeekday.getMonth(),
+                                yearlyWeekday.getWeekday(), yearlyWeekday.getWeekOfMonth());
                     }
                 }
-            }            
+            }
         } else {
             throw new IllegalStateException();
-        }        
+        }
     }
-    
+
     @Override
     public boolean hasNext() {
         return !currentDateTime.isAfter(endDateTime);
@@ -45,16 +47,16 @@ public class YearlyWeekdayIterator implements TimeSeriesIterator {
 
     @Override
     public Date next() {
-        if(this.hasNext()) {
+        if (this.hasNext()) {
             Date currentDate = currentDateTime.toDate();
-            currentDateTime = DateTimeUtils.getNthWeekdayOfMonth(currentDateTime.getYear() + yearIncrement, yearlyWeekday.getMonth(), 
-                            yearlyWeekday.getWeekday(), yearlyWeekday.getWeekOfMonth());
+            currentDateTime = DateTimeUtils.getNthWeekdayOfMonth(currentDateTime.getYear() + yearIncrement, yearlyWeekday.getMonth(),
+                    yearlyWeekday.getWeekday(), yearlyWeekday.getWeekOfMonth());
             return currentDate;
         }
-        
+
         throw new NoSuchElementException();
     }
-    
+
     @Override
     public void remove() {
         throw new UnsupportedOperationException();
